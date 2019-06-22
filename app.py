96a25200dcafbd229114da65a73e654320b762b8
Artit 'Art' Wangperawong <artitw@gmail.com>
2019-06-22 18:24:56 -0400

    master

--- app.py
+++ app.py
@@ -16,11 +16,25 @@ class User(db.Model):
 	email = db.Column(db.Text)
 	username = db.Column(db.Text)
 
+	def __init__(self, name, email, username):
+		self.name = name
+		self.email = email
+		self.username = username
+		
 db.create_all()
 
 @app.route('/')
 def index():
 	return "For the demo, navigate to the endpoint /"
+
+@app.route('/signup')
+def signup():
+	name = request.args.get('name')
+	email = request.args.get('email')
+	username = request.args.get('username')
+	new_user = User(name, email, username)
+	db.session.add(new_user)
+	return name + " was successfully added to the database!"
 
 if __name__ == "__main__":
 	app.run(debug=True, use_reloader=True)
